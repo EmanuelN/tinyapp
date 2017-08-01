9eafad0195ddccd4506065cfc51f108923f8b574
Emanuel Neves <emanuelneves10@gmail.com>
2017-08-01 20:18:54 +0000

    handles adding new elements to db

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 
+//allows us to access POST request parameters
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 //set view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -20,6 +24,39 @@ app.get("/", (req,res) =>{
 app.get('/urls', (req, res) =>{
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+//add urls page
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+//add urls POST
+app.post('/urls', (req, res) => {
+  console.log(req.body)
+  let id = generateRandomString()
+  urlDatabase[id] = req.body.longURL
+  let templateVars = { shortURL: id,
+    longURL: req.body.longURL};
+    //redirect to id's page
+  res.render('urls_show', templateVars);
+});
+
+//generate random String Function
+function generateRandomString(){
+  let randomStr = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++){
+    randomStr += possible.charAt(Math.floor(Math.random() * possible.length));
+  };
+  return randomStr;
+};
+
+//single url page
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+  longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 //about page
