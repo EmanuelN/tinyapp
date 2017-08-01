d5e6d5425b2af0f84c11ee67ca22ec49316764af
Emanuel Neves <emanuelneves10@gmail.com>
2017-08-01 17:43:38 +0000

    /urls_index returns a list of url ids and urls

--- server.js
+++ server.js
@@ -5,20 +5,21 @@ const app = express();
 //set view engine to ejs
 app.set('view engine', 'ejs');
 
+const urlDatabase = {
+  'b2xVn2': "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 //use res.render to load up an ejs view file
 
 //index page
-app.get('/', (req, res) =>{
-  let drinks = [
-    { name: 'Bloody Mary', drunkness: 3 },
-    { name: 'Martini', drunkness: 5},
-    { name: 'Scotch', drunkness: 10}
-  ];
-  let tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
-  res.render('pages/index', {
-    drinks: drinks,
-    tagline: tagline
-  });
+app.get("/", (req,res) =>{
+  res.end("Hello!\n");
+});
+
+//urls page
+app.get('/urls', (req, res) =>{
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 //about page
